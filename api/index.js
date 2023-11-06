327ea5bc630ae13632da69ff7202851db6446fc2
shobhit <guptashobhit2024@gmail.com>
2023-11-06 20:21:41 +0530

    6/11/23

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const salt = bcrypt.genSaltSync(10) //some random string to randominze the hashi
 //here 10 in bcrypt is that 10 rounds of hashing will take place by default it is 10
 const jwt = require('jsonwebtoken')
 const secret = 'asdfjkjlj3453' //secret code for jsonwebtoken
+const cookieParser = require('cookie-parser')//for parsing the cookie data to know that the user is loged in or not
 const { default: mongoose } = require('mongoose')
 const Usermodel = require('./models/User')
 const app = express()
 
-app.use(cors())
+app.use(cors({credentials:true, origin:'http://localhost:5173'})) //for allowing the credentials originated from the react localhost
 app.use(express.json()) //express json parser to convert data from json to object
+app.use(cookieParser())
 
 //database connection
 mongoose.connect('mongodb+srv://blog:sajcGJ2GSTke1jhh@cluster0.cnqeahp.mongodb.net/?retryWrites=true&w=majority')
@@ -40,6 +42,19 @@ app.post('/login', async(req,res)=>{
     }else{
         res.status(400).json('Wrong credentials')
     }
+})
+
+app.get('/profile', (req,res)=>{
+    const {token} = req.cookies
+    jwt.verify(token, secret, {}, (err, info)=>{ //it will verfy our token
+        if(err) throw err
+        res.json(info)
+    })
+    res.json(req.cookies)
+})
+
+app.post('/logout', (req,res)=>{
+    res.cookie('token', '').json('ok')
 })
 
 app.listen(4000, ()=>{
--- client/src/partials/Nav.jsx
+++ client/src/partials/Nav.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Nav() {
+  const [username, setUsername] = useState(null)
+  useEffect(()=>{
+    fetch('http://localhost:4000/profile',{
+      credentials: 'include',
+    }).then(response=>{
+      response.json().then(userInfo=>{
+        setUsername(userInfo.username)
+      })
+    })
+  },[])
+
+  function logout(){
+    fetch('http://localhost:4000/logout',{
+      credentials: 'include',
+      method: 'POST',
+    })
+    setUsername(null)
+  }
+
   return (
     <div>
     <main> 
@@ -16,8 +35,18 @@ function Nav() {
       <nav>
         <Link to="/">HomePage</Link>
         <Link to="">Contact </Link>
-        <Link to="/login">LogIn</Link>
-        <Link to="/signup">SignUp</Link>
+        {username &&(
+          <>
+            <Link to="/create" className='blog_item'>NEW-BLOG</Link>
+            <Link onClick={logout}>LogOut</Link>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">LogIn</Link>
+            <Link to="/signup">SignUp</Link>
+          </>
+        )}
       </nav>
       </header>
     </main>
